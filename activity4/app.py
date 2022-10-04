b64dd31a50da278b8042b2678eaf14dba25c4ddd
MrParba <101779137+MrParba@users.noreply.github.com>
2022-10-04 15:59:06 +0800

    Update app.py

--- activity4/app.py
+++ activity4/app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('base.html')
 
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port="8080", debug=True)
+app.run(host="localhost", debug=True)
